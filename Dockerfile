30186d61ed66ac80735b1daeffa6135a17d7fea6
Sebastien Goasguen <runseb@gmail.com>
2015-04-04 20:21:10 +0000

    working script

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,6 @@ RUN apt-get update && apt-get -y install \
 RUN apt-get clean
 RUN pip install docker-py
 
-ADD dockerplugin.py /opt/dockerplugin.py
-ADD dockerplugin.db /opt/dockerplugin.db
-ADD stat.py /opt/stat.py
 ADD docker-stats.py /opt/collectd/bin/docker-stats.py
 ADD docker-report.py /opt/collectd/bin/docker-report.py
 ADD collectd.conf /etc/collectd/collectd.conf
--- docker-stats.py
+++ docker-stats.py
@@ -8,17 +8,12 @@ import sys
 cli=docker.Client(base_url='unix://var/run/docker.sock')
  
 types = ["gauge-cpu0"]
-types = {"network": NetworkStats,
-         "blkio_stats": BlkioStats,
-         "cpu_stats": CpuStats,
-         "memory_stats": MemoryStats}
  
 for h in cli.containers():
     if not h["Status"].startswith("Up"):
         continue
     stats = json.loads(cli.stats(h["Id"]).next())
     for k, v in stats.items():
-        if types.get(k):
-            r = random.randrange(0, 100, 1)
-            print("PUTVAL %s/%s/%s N:%s" % (h, 'docker-cpu', t, r))
+        if k == "cpu_stats":
+            print("PUTVAL %s/%s/%s N:%s" % (h['Names'][0].lstrip('/'), 'docker-cpu', types[0], v['cpu_usage']['total_usage']))
 
